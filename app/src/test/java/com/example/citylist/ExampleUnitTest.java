36262f33a0056369ae74d2c899107718895f7e95
ishratafroz <103265005+ishratafroz@users.noreply.github.com>
2022-06-27 15:02:28 +0600

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/citylist/ExampleUnitTest.java
+++ app/src/test/java/com/example/citylist/ExampleUnitTest.java
@@ -8,7 +8,8 @@ import static org.junit.Assert.*;
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+test automation github actions unit test
+*/
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
